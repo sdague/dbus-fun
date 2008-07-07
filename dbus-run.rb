23e5362acea7497ae88258ff8844bc166277790b
Sean Dague <sean@dague.net>
2008-07-07 18:35:21 -0400

    restructure into a couple of methods for easier reading

--- dbus-run.rb
+++ dbus-run.rb
@@ -9,89 +9,92 @@ require "dbus/status"
 require "dbus/screensaver"
 require "dbus/network"
 
-system_bus = DBus::SystemBus.instance
-session_bus = DBus::SessionBus.instance
-
-pidgin = DBUS::Pidgin.new(session_bus)
-online = DBUS::Status.new(pidgin, "accounts.yaml")
-netman = DBUS::Network.new(system_bus)
-screensaver = DBUS::ScreenSaver.new(session_bus)
-
-@@msg = online.status
-if not @@msg
-    @@msg = pidgin.status.msg
-else
-    s = pidgin.status
-    s.msg = @@msg
-    pidgin.status = s
-end
-
-
-pidgin.on_signal("AccountStatusChanged") { |id, status, reason|
-    puts "AccountStatusChanged"
-    puts "Reason = #{reason}"
-    # this appears to be user changed
-    #    if reason == 1293
-    s = pidgin.status
-    if s.msg != "" and s.msg != @@msg
-        sync_status(pidgin, online)
-    end
-}
-
-pidgin.on_signal("SentImMsg") { |id, who, msg| 
-    begin 
-        pp pidgin.status
-        if pidgin.away?
-            puts "Setting status"
-            pidgin.status_msg(online.status)
-        end
-    rescue => e
-    end
-    puts "#{id} - #{who}: #{msg}"
-    pidgin.active!
-}
-
-# pidgin.on_signal("SentImMsg") { |id, who, msg|
-#     if who == "twitter@twitter.com" and !(msg =~ /^(invite|off|track|help|follow|whois|d \w+|whois)/)
-#         puts "setting twitter status"
-#         set_status(pidgin, 2, "Available", msg)
-#     else
-#         set_active(pidgin)
-#     end
-#     puts "#{id} - #{who}: #{msg}"
-# }
-
-# pidgin.on_signal("WroteImMsg") { |a, b, c, d|
-#     pp a
-#     pp b
-#     pp c
-#     pp d
-# }
-
-screensaver.on_signal("ActiveChanged") {|state|
-    if state
-        screensaver.mute
-        pidgin.away!
-    else
-        screensaver.unmute
-    end
-}
-
-netman.on_signal(system_bus, "DeviceNowActive") { |d, n|
-    puts "Device now active #{d} #{n}"
-    pidgin.reconnect
-    sync_status(pidgin, online)
-}
+@@msg = ""
 
 def sync_status(pidgin, online)
     begin 
-        @@msg = online.msg
+        puts "sync_status"
+        @@msg = online.status
         pidgin.status_msg(@@msg)
     rescue => e
+        puts "#{$!} => #{e}"
+        @@msg = pidgin.status.msg
     end
 end
 
-main = DBus::Main.new
-main << system_bus
-main << session_bus
-main.run
+def run()
+    system_bus = DBus::SystemBus.instance
+    session_bus = DBus::SessionBus.instance
+    
+    pidgin = DBUS::Pidgin.new(session_bus)
+    online = DBUS::Status.new(pidgin, "accounts.yaml")
+    netman = DBUS::Network.new(system_bus)
+    screensaver = DBUS::ScreenSaver.new(session_bus)
+    
+    sync_status(pidgin, online)
+
+    pidgin.on_signal("AccountStatusChanged") { |id, status, reason|
+        puts "AccountStatusChanged"
+        puts "Reason = #{reason}"
+        # this appears to be user changed
+        #    if reason == 1293
+        s = pidgin.status
+        if s.msg != "" and s.msg != @@msg
+            sync_status(pidgin, online)
+        end
+    }
+
+    pidgin.on_signal("SentImMsg") { |id, who, msg| 
+        begin 
+            pp pidgin.status
+            if pidgin.away?
+                puts "Setting status"
+                pidgin.status_msg(online.status)
+            end
+        rescue => e      
+            puts "#{$!} => #{e}"
+        end
+        puts "#{id} - #{who}: #{msg}"
+        pidgin.active!
+    }
+    
+    # pidgin.on_signal("SentImMsg") { |id, who, msg|
+    #     if who == "twitter@twitter.com" and !(msg =~ /^(invite|off|track|help|follow|whois|d \w+|whois)/)
+    #         puts "setting twitter status"
+    #         set_status(pidgin, 2, "Available", msg)
+    #     else
+    #         set_active(pidgin)
+    #     end
+    #     puts "#{id} - #{who}: #{msg}"
+    # }
+    
+    # pidgin.on_signal("WroteImMsg") { |a, b, c, d|
+    #     pp a
+    #     pp b
+    #     pp c
+    #     pp d
+    # }
+
+    screensaver.on_signal("ActiveChanged") {|state|
+        if state
+            screensaver.mute
+            pidgin.away!
+        else
+            screensaver.unmute
+        end
+    }
+    
+    netman.on_signal(system_bus, "DeviceNowActive") { |d, n|
+        puts "Device now active #{d} #{n}"
+        pidgin.reconnect
+        sync_status(pidgin, online)
+    }
+
+    main = DBus::Main.new
+    main << system_bus
+    main << session_bus
+    main.run
+end
+
+# and actually run the program
+run
